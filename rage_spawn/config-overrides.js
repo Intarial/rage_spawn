eed80489d272c8c18b94f394b31dd9d9848a4fc3
Nekogl <99093592+Intarial@users.noreply.github.com>
2023-03-17 13:58:50 +0300

    initialization project

---
+++ rage_spawn/config-overrides.js
@@ -0,0 +1,3 @@
+const { alias, configPaths } = require('react-app-rewire-alias');
+const aliasMap = configPaths('./tsconfig.paths.json');
+module.exports = alias(aliasMap);
---
+++ rage_spawn/src/Spawn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Spawn.module.scss';
+
+function Spawn() {
+  return (
+    <div className={ styles.wrapper }>
+    </div>
+  );
+}
+
+export default Spawn;
